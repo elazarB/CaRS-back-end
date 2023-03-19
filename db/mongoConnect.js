c08a98c0c9c2659f3b6bacc83d362ecc775b5791
elazar ben chaem <120422631+elazarB@users.noreply.github.com>
2023-03-19 22:26:34 +0200

    Update mongoConnect.js

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,7 +6,7 @@ async function main() {
   // כדי למנוע הצגת אזהרה
   mongoose.set('strictQuery', false);
   // וזה לווינדוס 11
-  await mongoose.connect('mongodb://127.0.0.1:27017/cars');
+  await mongoose.connect('mongodb+srv://CaRS:cars1234@cluster0.ibm2hzi.mongodb.net/test');
   console.log("mongo connect cars local");
   
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
