140db58d009674e83c41be4c171f1ca64a8d68a0
elazar ben chaem <120422631+elazarB@users.noreply.github.com>
2023-03-19 22:35:53 +0200

    Add files via upload

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,7 +6,7 @@ async function main() {
   // כדי למנוע הצגת אזהרה
   mongoose.set('strictQuery', false);
   // וזה לווינדוס 11
-  await mongoose.connect('mongodb+srv://CaRS:cars1234@cluster0.ibm2hzi.mongodb.net/test');
+  await mongoose.connect('mongodb+srv://CaRS:cars1234@cluster0.ibm2hzi.mongodb.net/cars');
   console.log("mongo connect cars atlas");
   
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
