47f29e1de16de8585d0421c843edade1b42a01b1
elazar ben chaem <120422631+elazarB@users.noreply.github.com>
2023-05-15 13:25:38 +0300

    Update interactionsModel.js

--- models/interactionsModel.js
+++ models/interactionsModel.js
@@ -24,7 +24,7 @@ exports.validateInteractions = (_reqBody) => {
   return Joi.object({
     tenant_name: Joi.object().min(1).max(150).required(),
     category: Joi.string().min(1).max(150).required(),
-    car_id: Joi.object().min(0).max(150).allow(null,""),
+    car_id: Joi.object().min(1).max(150).allow(null,""),
     license_number: Joi.string().min(2).max(150).required(),
     pick_up_date: Joi.date().min("1-1-2023").max("1-1-2100").required(),
     pick_up_time: Joi.string().min(2).max(150).required(),
