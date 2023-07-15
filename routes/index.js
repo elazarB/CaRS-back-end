3a60751f0acca22a40db885f5ce2425cbc03573d
elazar ben chaem <120422631+elazarB@users.noreply.github.com>
2023-07-15 23:07:03 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
 
-router.get("/", async(req,res) => {
-  res.json({msg:"cars Api Work!!!"});
+router.get("/", async (req, res) => {
+  res.json({ msg: "cars Api Work!!!" });
 })
 
 
